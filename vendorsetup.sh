3416c02f1a319b3ad1cd43e01af1cb11c8053712
hoshiyomiX <138315928+hoshiyomiX@users.noreply.github.com>
2023-09-18 18:48:29 +0800

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -13,16 +13,14 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-
 FDEVICE="x695c"
-#set -o xtrace
 
 fox_get_target_device() {
-local chkdev=$(echo "$BASH_SOURCE" | grep -w $FDEVICE)
-   if [ -n "$chkdev" ]; then 
+local chkdev=$(echo "$BASH_SOURCE" | grep $FDEVICE)
+   if [ -n "$chkdev" ]; then
       FOX_BUILD_DEVICE="$FDEVICE"
    else
-      chkdev=$(set | grep BASH_ARGV | grep -w $FDEVICE)
+      chkdev=$(set | grep BASH_ARGV | grep $FDEVICE)
       [ -n "$chkdev" ] && FOX_BUILD_DEVICE="$FDEVICE"
    fi
 }
@@ -32,43 +30,72 @@ if [ -z "$1" -a -z "$FOX_BUILD_DEVICE" ]; then
 fi
 
 if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
-
-    export FOX_VERSION="R11.1"
-    export FOX_BUILD_TYPE=Unofficial
-    # export FOX_ENABLE_LAB=1
-    
-    export TARGET_DEVICE_ALT="Infinix-X695C,X695C"
-      export OF_MAINTAINER="hoshiyomi"
-
-    export OF_AB_DEVICE=1
-    export OF_USE_MAGISKBOOT=1
-    export OF_USE_MAGISKBOOT_FOR_ALL_PATCHES=1
-
-    export OF_DISABLE_MIUI_SPECIFIC_FEATURES=1
-    export OF_SUPPORT_ALL_BLOCK_OTA_UPDATES=0
-
-    export FOX_USE_NANO_EDITOR=1
-    export FOX_USE_BASH_SHELL=1
-    export FOX_USE_TAR_BINARY=1
+    export TW_DEFAULT_LANGUAGE="en"
+    export OF_DEVICE_ALT="X695C,Infinix-X695C"
+	export LC_ALL="C"
+	export OF_AB_DEVICE=1
+	export FOX_EXTREME_SIZE_REDUCTION=1
+	export ALLOW_MISSING_DEPENDENCIES=true
+	export OF_QUICK_BACKUP_LIST="/data;"
+	export OF_USE_MAGISKBOOT=1
+	export OF_USE_MAGISKBOOT_FOR_ALL_PATCHES=1
+	export FOX_USE_TWRP_RECOVERY_IMAGE_BUILDER=1
+	export OF_NO_TREBLE_COMPATIBILITY_CHECK=1
+	export OF_DONT_PATCH_ENCRYPTED_DEVICE=1
+	export FOX_USE_BASH_SHELL=1
+	export FOX_ASH_IS_BASH=1
+	export FOX_USE_NANO_EDITOR=1
+	export OF_SKIP_MULTIUSER_FOLDERS_BACKUP=1
+	export OF_CLOCK_POS=1
+	export FOX_DELETE_AROMAFM=1
+	export OF_USE_GREEN_LED=0
+	export FOX_ENABLE_APP_MANAGER=1
     export FOX_USE_XZ_UTILS=1
+	
+	# OTA
+    #export OF_SUPPORT_ALL_BLOCK_OTA_UPDATES=1
+    export OF_FIX_OTA_UPDATE_MANUAL_FLASH_ERROR=1
 
-    export OF_ADVANCED_SECURITY=1
+	# encryption
+	export OF_DISABLE_FORCED_ENCRYPTION=1
+	export OF_DISABLE_DM_VERITY_FORCED_ENCRYPTION=1
+	
+	# screen settings
+	export OF_SCREEN_H=2340
+	export OF_STATUS_H=80
+	export OF_CLOCK_POS=2
+	export OF_STATUS_INDENT_LEFT=48
+	export OF_STATUS_INDENT_RIGHT=48
 
-    # Temp
-    export OF_NO_SPLASH_CHANGE=1
+	# Bootimage Partition path
+	export FOX_RECOVERY_BOOT_PARTITION="/dev/block/by-name/boot"
+	export FOX_RECOVERY_VENDOR_PARTITION="/dev/block/mapper/vendor"
+	export FOX_RECOVERY_SYSTEM_PARTITION="/dev/block/mapper/system"
+	
+	# flashlight
+	export OF_FLASHLIGHT_ENABLE=0
 
-    export FOX_REMOVE_AAPT=1
-    export FOX_DISABLE_APP_MANAGER=1
-    export FOX_DELETE_AROMAFM=1
+	# Android 12 Encryption *temporary use until binary.xml file will implement* #
+	export OF_SKIP_DECRYPTED_ADOPTED_STORAGE=1
+	export OF_SKIP_FBE_DECRYPTION_SDKVERSION=34 # Don't try to decrypt A14(?)
+    
+	# R11
+	export FOX_VERSION="0.0.1"
+	export FOX_VARIANT=Alpha
+	export OF_PATCH_AVB20=1
+	export OF_MAINTAINER="hoshiyomi"
+	export FOX_ADVANCED_SECURITY=1
+	export OF_USE_TWRP_SAR_DETECT=1
 
-    export OF_STATUS_INDENT_LEFT=64
-    export OF_STATUS_INDENT_RIGHT=64
+	# run a process after formatting data to work-around MTP issues
+	export OF_RUN_POST_FORMAT_PROCESS=1
 
-    export FOX_BUGGED_AOSP_ARB_WORKAROUND="1546300800"; # Tuesday, January 1, 2019 12:00:00 AM GMT+00:00
+	if [ -n "$FOX_BUILD_LOG_FILE" -a -f "$FOX_BUILD_LOG_FILE" ]; then
+  	   export | grep "FOX" >> $FOX_BUILD_LOG_FILE
+  	   export | grep "OF_" >> $FOX_BUILD_LOG_FILE
+  	   export | grep "TARGET_" >> $FOX_BUILD_LOG_FILE
+  	   export | grep "TW_" >> $FOX_BUILD_LOG_FILE
+ 	fi
 
-
-    # export FOX_BOARD_AVB_ENABLE=1
-    # export AVBTOOL=external/avb
 fi
 #
-
