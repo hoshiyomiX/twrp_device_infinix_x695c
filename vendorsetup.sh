08f56a648de8ffa1f0d52f4fa9a4e4025e57c9d3
hoshiyomiX <138315928+hoshiyomiX@users.noreply.github.com>
2023-09-18 16:01:21 +0800

    Add files via upload

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,26 +1,20 @@
-#!/usr/bin/env bash
 #
-#	This file is part of the OrangeFox Recovery Project
-# 	Copyright (C) 2020-2021 The OrangeFox Recovery Project
+# Copyright (C) 2023 The OrangeFox Recovery Project
 #
-#	OrangeFox is free software: you can redistribute it and/or modify
-#	it under the terms of the GNU General Public License as published by
-#	the Free Software Foundation, either version 3 of the License, or
-#	any later version.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
 #
-#	OrangeFox is distributed in the hope that it will be useful,
-#	but WITHOUT ANY WARRANTY; without even the implied warranty of
-#	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#	GNU General Public License for more details.
+# http://www.apache.org/licenses/LICENSE-2.0
 #
-# 	This software is released under GPL version 3 or any later version.
-#	See <http://www.gnu.org/licenses/>.
-#
-# 	Please maintain this if you use this script or any part of it
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 #
 
-
-FDEVICE="x695c"
+FDEVICE="X6812"
 #set -o xtrace
 
 fox_get_target_device() {
@@ -37,60 +31,44 @@ if [ -z "$1" -a -z "$FOX_BUILD_DEVICE" ]; then
    fox_get_target_device
 fi
 
-# Dirty Fix: Only declare orangefox vars when needed
-if [ -f "$(gettop)/bootable/recovery/orangefox.cpp" ]
-then
-	echo -e "\x1b[96mSetting up OrangeFox build vars...\x1b[m"
-	if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
-   		export TW_DEFAULT_LANGUAGE="en"
-		export LC_ALL="C"
-	 	export OF_FLASHLIGHT_ENABLE=0
-		export ALLOW_MISSING_DEPENDENCIES=true
-		export OF_USE_GREEN_LED=0
-		export OF_HIDE_NOTCH=1
-		export OF_MAINTAINER="Woomymy"
-		export OF_USE_MAGISKBOOT=1
-		export OF_USE_MAGISKBOOT_FOR_ALL_PATCHES=1
-		export OF_DONT_PATCH_ENCRYPTED_DEVICE=1
-		export OF_NO_TREBLE_COMPATIBILITY_CHECK=1
-		export OF_NO_MIUI_PATCH_WARNING=1
-		export FOX_USE_BASH_SHELL=1
-		export FOX_ASH_IS_BASH=1
-		export FOX_USE_TAR_BINARY=1
-		export FOX_USE_SED_BINARY=1
-		export FOX_USE_XZ_UTILS=1
-		export OF_SKIP_MULTIUSER_FOLDERS_BACKUP=1
-	    	export OF_USE_TWRP_SAR_DETECT=1
-		export OF_NO_SAMSUNG_SPECIAL=1
-		export OF_QUICK_BACKUP_LIST="/boot;/data;"
-	    export FOX_DELETE_AROMAFM=1
-	    export FOX_ENABLE_APP_MANAGER=1
-		export FOX_USE_NANO_EDITOR=1
-	    # OTA
-	    export OF_KEEP_DM_VERITY=1
-	    export OF_SUPPORT_ALL_BLOCK_OTA_UPDATES=1
-	    export OF_FIX_OTA_UPDATE_MANUAL_FLASH_ERROR=1
-	    export OF_DISABLE_MIUI_OTA_BY_DEFAULT=1
-		export OF_USE_NEW_MAGISKBOOT=1
-		export OF_ENABLE_LPTOOLS=1
-		# screen settings
-		export OF_SCREEN_H=2460
-		export OF_STATUS_H=100
-		export OF_ALLOW_DISABLE_NAVBAR=0 # We don't have physical buttons
-		export OF_STATUS_INDENT_LEFT=48
-		export OF_STATUS_INDENT_RIGHT=48
-		export OF_CLOCK_POS=1
-		# maximum permissible splash image size (in kilobytes); do *NOT* increase!
-		export OF_SPLASH_MAX_SIZE=104
-		export OF_PATCH_AVB20=1
-		export FOX_VERSION=R11.1_1
-		# run a process after formatting data to work-around MTP issues
-		export FOX_BUGGED_AOSP_ARB_WORKAROUND="1546300800"
-		export FOX_RECOVERY_BOOT_PARTITION="/dev/block/by-name/boot"
-		export OF_AB_DEVICE=1
-	export OF_DONT_PATCH_ON_FRESH_INSTALLATION=1
-		export FOX_USE_SPECIFIC_MAGISK_ZIP="$(gettop)/device/infinix/x695c/Magisk/Magisk.zip"
-	fi
+if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
+
+    export FOX_VERSION="R11.1"
+    export FOX_BUILD_TYPE=Unofficial
+    # export FOX_ENABLE_LAB=1
+    
+    export TARGET_DEVICE_ALT="Infinix-X6812,X6812"
+      export OF_MAINTAINER="aminegames125"
+
+    export OF_AB_DEVICE=1
+    export OF_USE_MAGISKBOOT=1
+    export OF_USE_MAGISKBOOT_FOR_ALL_PATCHES=1
+
+    export OF_DISABLE_MIUI_SPECIFIC_FEATURES=1
+    export OF_SUPPORT_ALL_BLOCK_OTA_UPDATES=0
+
+    export FOX_USE_NANO_EDITOR=1
+    export FOX_USE_BASH_SHELL=1
+    export FOX_USE_TAR_BINARY=1
+    export FOX_USE_XZ_UTILS=1
+
+    export OF_ADVANCED_SECURITY=1
+
+    # Temp
+    export OF_NO_SPLASH_CHANGE=1
+
+    export FOX_REMOVE_AAPT=1
+    export FOX_DISABLE_APP_MANAGER=1
+    export FOX_DELETE_AROMAFM=1
+
+    export OF_STATUS_INDENT_LEFT=64
+    export OF_STATUS_INDENT_RIGHT=64
+
+    export FOX_BUGGED_AOSP_ARB_WORKAROUND="1546300800"; # Tuesday, January 1, 2019 12:00:00 AM GMT+00:00
+
+
+    # export FOX_BOARD_AVB_ENABLE=1
+    # export AVBTOOL=external/avb
 fi
 #
 
